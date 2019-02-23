97a3e2d25739e5d778ea755e8d66970d2e61a7ae
kun0620 <41094738+kun0620@users.noreply.github.com>
2019-02-23 11:50:24 +0700

    add router hompage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  {path: 'tab1', loadChildren: './tab1/tab2.module#Tab2PageModule' },
+  {path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
+  {path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' },
+  {path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
+  {path: 'tab5', loadChildren: './tab5/tab5.module#Tab5PageModule' },
+
+  
+
 ];
 @NgModule({
   imports: [
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+constructor(private router: Router) {
+
+}
+gohtml(){
+  this.router.navigateByUrl('/tabs/tab2');
+ }
+ gocss(){
+   this.router.navigateByUrl('/tabs/tab3');
+  }
+  gosql(){
+    this.router.navigateByUrl('/tabs/tab4');
+   }
+   gophp(){
+     this.router.navigateByUrl('/tabs/tab5');
+    }
+
+}
