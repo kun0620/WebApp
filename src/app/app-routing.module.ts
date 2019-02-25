ae295f9446f6740a3b02b7972eb24462125564b6
kun0620 <41094738+kun0620@users.noreply.github.com>
2019-02-25 19:34:32 +0700

    5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  {path: 'tab1', loadChildren: './tab1/tab2.module#Tab2PageModule' },
+  {path: 'tab1', loadChildren: './tab1/tab1.module#Tab2PageModule' },
   {path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
   {path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' },
   {path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
