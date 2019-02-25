ac8c3a6401b19bdc468fac9276c3f7a8d9b82263
kun0620 <41094738+kun0620@users.noreply.github.com>
2019-02-25 19:52:28 +0700

    goo

---
+++ src/app/tab5/phpdat/phpdat.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PhpdatPage } from './phpdat.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PhpdatPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [PhpdatPage]
+})
+export class PhpdatPageModule {}
---
+++ src/app/tab5/phpdat/phpdat.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phpdat',
+  templateUrl: './phpdat.page.html',
+  styleUrls: ['./phpdat.page.scss'],
+})
+export class PhpdatPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
